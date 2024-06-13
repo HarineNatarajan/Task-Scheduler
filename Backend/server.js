043cf12d2eeee6bd0a9e7b7c98c147c60b601efc
Harine N <149371960+HarineNatarajan@users.noreply.github.com>
2024-06-13 11:47:16 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,64 +1,36 @@
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
-const app = express();
-const PORT = 5000;
+const mongoose = require('mongoose');
 const cors = require('cors');
+const taskRoutes = require('./routes/taskRoutes');
+const app = express();
 
-// Sample database of tasks
-let tasks = [];
-app.use(express.json());
+// Middleware
 app.use(cors());
-// Get all  tasks
-app.get('/tasks', (req, res) => {
-  res.send(tasks);
-});
+app.use(express.json());
 
-// Get a specific tasks by ID
-app.get('/tasks/:id', (req, res) => {
-  const postId = req.params.id;
-  const post = tasks.find(post => post.id === postId);
-  if (!post) {
-    return res.status(404).json({ message: 'Post not found' });
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error(error);
+    process.exit(1); // Exit process with failure
   }
-  res.json(post);
-});
+};
 
-// Create a new task
-app.post('/tasks', (req, res) => {
-  const { title, date, time } = req.body;
-  const newTask = { id: Date.now().toString(), title, date, time, completed: false };
-  tasks.push(newTask);
-  res.status(201).json(newTask);
-});
+// Connect to the database
+connectDB();
 
-// Update an existing tasks
-app.put('/tasks/:id', (req, res) => {
-  const taskId = req.params.id;
-  const { title, date, time } = req.body;
-  const taskIndex = tasks.findIndex(task => task.id === taskId);
-  if (taskIndex === -1) {
-    return res.status(404).json({ message: 'Task not found' });
-  }
-  tasks[taskIndex] = { ...tasks[taskIndex], title, date, time };
-  res.json(tasks[taskIndex]);
-});
+// Use task routes
+app.use("/api", taskRoutes);
 
-// Mark a task as complete
-app.put('/tasks/:id/complete', (req, res) => {
-  const taskId = req.params.id;
-  const taskIndex = tasks.findIndex(task => task.id === taskId);
-  if (taskIndex === -1) {
-    return res.status(404).json({ message: 'Task not found' });
-  }
-  tasks[taskIndex].completed = true;
-  res.json(tasks[taskIndex]);
-});
-
-// Delete an existing tasks
-app.delete('/tasks/:id', (req, res) => {
-  const taskId = req.params.id;
-  tasks = tasks.filter(task => task.id !== taskId);
-  res.status(204).send();
-});
+// Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
